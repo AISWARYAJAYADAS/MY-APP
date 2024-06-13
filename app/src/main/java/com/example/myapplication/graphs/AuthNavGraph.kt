e5a1aba570858ba5d2691c6c59f879ab50711670
aiswarya-jayadas <aiswarya.jayadas@qburst.com>
2024-06-13 10:51:09 +0530

    updated code

--- app/src/main/java/com/example/myapplication/graphs/AuthNavGraph.kt
+++ app/src/main/java/com/example/myapplication/graphs/AuthNavGraph.kt
@@ -5,12 +5,9 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
-import com.example.myapplication.api.RefreshTokenApiService
 import com.example.myapplication.login.LoginScreen
 import com.example.myapplication.splash.SplashScreen
 import com.example.myapplication.splash.SplashViewModel
-import com.example.myapplication.utils.manager.TokenExpiryManager
-import javax.inject.Inject
 
 fun NavGraphBuilder.authNavGraph(navController: NavHostController) {
     navigation(
